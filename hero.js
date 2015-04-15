1be925afc2365532fb1d7727f2deb86237aa6804
limingth <limingth@gmail.com>
2015-04-15 17:08:07 +0800

    Change hero to maodou-doodle.meteor.com

--- hero.js
+++ hero.js
@@ -13,22 +13,44 @@ if (Meteor.isClient) {
 
   Template.hero.events({
     'click .up': function () {
-      Players.update(Session.get("selectedPlayer"), {$inc: {score: 5}});
-    },
-    'click .del': function () {
-      Players.remove(Session.get("selectedPlayer"));
+      Players.update(Session.get("selectedPlayer"), {$inc: {vote: 1}});
+      
+      var voted = [];
+      voted.push("abc");
+      voted.push("123");
+      voted.push("789");
+
+      Session.set("votedPlayers", voted);
+      console.log('voted :' + Session.get("votedPlayers"));
+      return;
+
+
+      if (Session.get("votedPlayer") != undefined)
+        voted.push(Session.get("votedPlayer"));
+      voted.push(Session.get("selectedPlayer"));
+      voted = ["abcd", "1234"];
+      voted.push("abc");
+      voted.push("123");
+      console.log('here: ', voted);
+      Session.set("votedPlayers", voted);
+      console.log('voted :' + Session.get("votedPlayers"));
     },
     'click .down': function () {
-      Players.update(Session.get("selectedPlayer"), {$inc: {score: -5}});
+      Players.update(Session.get("selectedPlayer"), {$inc: {unvote: 1}});
     },
+   'click .del': function () {
+      Players.remove(Session.get("selectedPlayer"));
+    }, 
     'click .name': function () {
       Players.update(Session.get("selectedPlayer"), {$inc: {score: -5}});
     }
   });
 
   Template.player.events({
-    'click': function () {
+    'click .player': function (event) {
       Session.set("selectedPlayer", this._id);
+      console.log('click voted :' + Session.get("votedPlayers"));
+      $(event.target).css( "background-color", "#6ffff4" );
     }
   });
 
@@ -38,7 +60,9 @@ if (Meteor.isClient) {
 
       Players.insert({
         name: name,
-        score: Math.floor(Random.fraction() * 10) * 5
+        //score: Math.floor(Random.fraction() * 10) * 5
+        vote: 0,
+        unvote: 0,
       });
 
       event.target.name.value = "";
@@ -59,7 +83,9 @@ if (Meteor.isServer) {
       _.each(names, function (name) {
         Players.insert({
           name: name,
-          score: Math.floor(Random.fraction() * 10) * 5
+          //score: Math.floor(Random.fraction() * 10) * 5
+          vote: 0,
+          unvote: 0,
         });
       });
     }
