69aecad1a57f1c489c447f0f4e015775c08863a6
limingth <limingth@gmail.com>
2015-05-07 12:02:06 +0800

    Add all doodle files

--- hero.js
+++ hero.js
@@ -1,45 +1,35 @@
 Players = new Mongo.Collection("players");
 
 if (Meteor.isClient) {
+  Session.set("clickName", 0);
+  Session.set("showDel", false);
+
   Template.hero.helpers({
     players: function () {
-      return Players.find({}, { sort: { score: -1, name: 1}});
+      return Players.find({}, { sort: { vote: -1, name: 1}});
     },
     selectedName: function () {
       var player = Players.findOne(Session.get("selectedPlayer"));
       return player && player.name;
+    },
+    showDel: function (argument) {
+      return Session.get("showDel");
     }
   });
 
   Template.hero.events({
     'click .up': function () {
       Players.update(Session.get("selectedPlayer"), {$inc: {vote: 1}});
-      
-      var voted = [];
-      voted.push("abc");
-      voted.push("123");
-      voted.push("789");
-
-      Session.set("votedPlayers", voted);
-      console.log('voted :' + Session.get("votedPlayers"));
-      return;
-
-
-      if (Session.get("votedPlayer") != undefined)
-        voted.push(Session.get("votedPlayer"));
-      voted.push(Session.get("selectedPlayer"));
-      voted = ["abcd", "1234"];
-      voted.push("abc");
-      voted.push("123");
-      console.log('here: ', voted);
-      Session.set("votedPlayers", voted);
-      console.log('voted :' + Session.get("votedPlayers"));
+      Session.set("clickName", 0);
+      Session.set("showDel", false);
     },
     'click .down': function () {
       Players.update(Session.get("selectedPlayer"), {$inc: {unvote: 1}});
     },
    'click .del': function () {
       Players.remove(Session.get("selectedPlayer"));
+      Session.set("showDel", false);
+      Session.set("clickName", 0);
     }, 
     'click .name': function () {
       Players.update(Session.get("selectedPlayer"), {$inc: {score: -5}});
@@ -50,8 +40,18 @@ if (Meteor.isClient) {
     'click .player': function (event) {
       Session.set("selectedPlayer", this._id);
       console.log('click voted :' + Session.get("votedPlayers"));
-      $(event.target).css( "background-color", "#6ffff4" );
-    }
+
+      $('.leaderboard li').css( "background-color", "white" );
+      //$(event.target).css( "background-color", "#6ffff4" );
+      $(event.target).closest('li.player').css( "background-color", "#6ffff4" );  
+    },
+    'click .name': function (event) {
+      console.log('clickName :', Session.get("clickName"));
+      Session.set("clickName", Session.get("clickName") + 1);
+
+      if (Session.get("clickName") > 5)
+        Session.set("showDel", true);
+    },
   });
 
   Template.body.events({
@@ -66,7 +66,7 @@ if (Meteor.isClient) {
       });
 
       event.target.name.value = "";
-
+      Session.set("clickName", 0);
       return false;
     }
   });
